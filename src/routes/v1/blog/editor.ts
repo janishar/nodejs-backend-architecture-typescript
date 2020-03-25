bcfb9386a10e1e57da8246b9f59d4e49977aad97
Janishar Ali <janishar.ali@gmail.com>
2020-03-25 15:44:40 +0530

    add blog editor apis

---
+++ src/routes/v1/blog/editor.ts
@@ -0,0 +1,90 @@
+import express from 'express';
+import { BadRequestResponse, SuccessResponse, SuccessMsgResponse } from '../../../utils/ApiResponse';
+import { ProtectedRequest, RoleRequest } from 'app-request';
+import { BadRequestError, ForbiddenError } from '../../../utils/ApiError';
+import BlogRepo from '../../../database/repository/BlogRepo';
+import { RoleCode } from '../../../database/model/Role';
+import { Types } from 'mongoose';
+import validator, { ValidationSource } from '../../../helpers/validator';
+import schema from './schema';
+import asyncHandler from '../../../helpers/asyncHandler';
+import _ from 'lodash';
+
+const router = express.Router();
+
+/*-------------------------------------------------------------------------*/
+// Below all APIs are private APIs protected for Access Token and Editor's Role
+router.use('/',
+	require('../../../auth/Authentication'),
+	(req: RoleRequest, res, next) => { req.currentRoleCode = RoleCode.EDITOR; next(); },
+	require('../../../auth/Authorization'));
+/*-------------------------------------------------------------------------*/
+
+router.put('/publish/:id', validator(schema.blogId, ValidationSource.PARAM),
+	asyncHandler(async (req: ProtectedRequest, res, next) => {
+		const blog = await BlogRepo.findBlogAllDataById(new Types.ObjectId(req.params.id));
+		if (!blog) throw new BadRequestError('Blog does not exists');
+
+		blog.isDraft = false;
+		blog.isSubmitted = false;
+		blog.isPublished = true;
+		blog.text = blog.draftText;
+		if (!blog.publishedAt) blog.publishedAt = new Date();
+
+		await BlogRepo.update(blog);
+		return new SuccessMsgResponse('Blog published successfully').send(res);
+	}));
+
+router.put('/unpublish/:id', validator(schema.blogId, ValidationSource.PARAM),
+	asyncHandler(async (req: ProtectedRequest, res, next) => {
+		const blog = await BlogRepo.findBlogAllDataById(new Types.ObjectId(req.params.id));
+		if (!blog) throw new BadRequestError('Blog does not exists');
+
+		blog.isDraft = true;
+		blog.isSubmitted = false;
+		blog.isPublished = false;
+
+		await BlogRepo.update(blog);
+		return new SuccessMsgResponse('Blog unpublished successfully').send(res);
+	}));
+
+router.delete('/id/:id', validator(schema.blogId, ValidationSource.PARAM),
+	asyncHandler(async (req: ProtectedRequest, res, next) => {
+		const blog = await BlogRepo.findBlogAllDataById(new Types.ObjectId(req.params.id));
+		if (!blog) throw new BadRequestError('Blog does not exists');
+
+		blog.status = false;
+
+		await BlogRepo.update(blog);
+		return new SuccessMsgResponse('Blog deleted successfully').send(res);
+	}));
+
+router.get('/published/all',
+	asyncHandler(async (req: ProtectedRequest, res, next) => {
+		const blogs = await BlogRepo.findAllPublished();
+		return new SuccessResponse('success', blogs).send(res);
+	}));
+
+router.get('/submitted/all',
+	asyncHandler(async (req: ProtectedRequest, res, next) => {
+		const blogs = await BlogRepo.findAllSubmissions();
+		return new SuccessResponse('success', blogs).send(res);
+	}));
+
+router.get('/drafts/all',
+	asyncHandler(async (req: ProtectedRequest, res, next) => {
+		const blogs = await BlogRepo.findAllDrafts();
+		return new SuccessResponse('success', blogs).send(res);
+	}));
+
+router.get('/id/:id', validator(schema.blogId, ValidationSource.PARAM),
+	asyncHandler(async (req: ProtectedRequest, res, next) => {
+		const blog = await BlogRepo.findBlogAllDataById(new Types.ObjectId(req.params.id));
+
+		if (blog == null) throw new BadRequestError('Blog does not exists');
+		if (!blog.isSubmitted && !blog.isPublished) throw new ForbiddenError('This blog is private');
+
+		new SuccessResponse('success', blog).send(res);
+	}));
+
+module.exports = router;
