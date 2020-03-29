a3ddc93459382355abcc6c3f11f7a487c03a1457
Janishar Ali <janishar.ali@gmail.com>
2020-03-29 17:50:09 +0530

    update test coverage settings

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	setupFiles: ['<rootDir>/tests/setup.ts']
+	roots: ['<rootDir>/tests'],
+	setupFiles: ['<rootDir>/tests/setup.ts'],
+	"collectCoverageFrom": [
+		"<rootDir>/src/**/*.ts",
+		"!**/node_modules/**",
+	]
 };
