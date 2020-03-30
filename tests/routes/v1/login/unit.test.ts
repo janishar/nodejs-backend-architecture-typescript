1aeca5287be860b9d08e5da608a3d216729c543e
Janishar Ali <janishar.ali@gmail.com>
2020-03-31 04:31:25 +0530

    add login integration test

--- tests/routes/v1/login/unit.test.ts
+++ tests/routes/v1/login/unit.test.ts
@@ -9,7 +9,7 @@ import {
 import supertest from 'supertest';
 import app from '../../../../src/app';
 
-describe('Login basic route', () => {
+describe('Unit Test: Login basic route', () => {
 
 	const endpoint = '/v1/login/basic';
 	const request = supertest(app);
