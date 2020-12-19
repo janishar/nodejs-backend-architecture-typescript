f240b1f2266bb84171ac9eb062d19a29046bb6d3
lita <luisorbaiceta@gmail.com>
2020-12-19 12:48:37 +0100

    #20 asyncHandler.ts fix

--- src/helpers/asyncHandler.ts
+++ src/helpers/asyncHandler.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
-type AsyncFunction = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+type AsyncFunction<T extends Request> = (req: T, res: Response, next: NextFunction) => Promise<any>;
 
-export default (execution: AsyncFunction) => (req: Request, res: Response, next: NextFunction) => {
+export default (execution: AsyncFunction<Request>) => (req: Request, res: Response, next: NextFunction) => {
   execution(req, res, next).catch(next);
 };
