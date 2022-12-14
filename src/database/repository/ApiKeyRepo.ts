359101f400a2683d12d2dd49f8d8df9c1355f9c4
Janishar Ali <janishar.ali@gmail.com>
2022-12-14 17:35:49 +0530

    migrate static class to modules export

--- src/database/repository/ApiKeyRepo.ts
+++ src/database/repository/ApiKeyRepo.ts
@@ -1,7 +1,9 @@
 import ApiKey, { ApiKeyModel } from '../model/ApiKey';
 
-export default class ApiRepo {
-  public static findByKey(key: string): Promise<ApiKey | null> {
-    return ApiKeyModel.findOne({ key: key, status: true }).lean().exec();
-  }
+async function findByKey(key: string): Promise<ApiKey | null> {
+  return ApiKeyModel.findOne({ key: key, status: true }).lean().exec();
 }
+
+export default {
+  findByKey,
+};
--- src/database/repository/UserRepo.ts
+++ src/database/repository/UserRepo.ts
@@ -5,98 +5,108 @@ import { Types } from 'mongoose';
 import KeystoreRepo from './KeystoreRepo';
 import Keystore from '../model/Keystore';
 
-export default class UserRepo {
-  public static async exists(id: Types.ObjectId): Promise<boolean> {
-    const user = await UserModel.exists({ _id: id, status: true });
-    return user !== null && user !== undefined;
-  }
-
-  public static findPrivateProfileById(id: Types.ObjectId): Promise<User | null> {
-    return UserModel.findOne({ _id: id, status: true })
-      .select('+email')
-      .populate({
-        path: 'roles',
-        match: { status: true },
-        select: { code: 1 },
-      })
-      .lean<User>()
-      .exec();
-  }
-
-  // contains critical information of the user
-  public static findById(id: Types.ObjectId): Promise<User | null> {
-    return UserModel.findOne({ _id: id, status: true })
-      .select('+email +password +roles')
-      .populate({
-        path: 'roles',
-        match: { status: true },
-      })
-      .lean()
-      .exec();
-  }
-
-  public static findByEmail(email: string): Promise<User | null> {
-    return UserModel.findOne({ email: email })
-      .select(
-        '+email +password +roles +gender +dob +grade +country +state +city +school +bio +hobbies',
-      )
-      .populate({
-        path: 'roles',
-        match: { status: true },
-        select: { code: 1 },
-      })
-      .lean()
-      .exec();
-  }
-
-  public static findFieldsById(id: Types.ObjectId, ...fields: string[]): Promise<User | null> {
-    return UserModel.findOne({ _id: id, status: true }, [...fields])
-      .lean()
-      .exec();
-  }
-
-  public static findPublicProfileById(id: Types.ObjectId): Promise<User | null> {
-    return UserModel.findOne({ _id: id, status: true }).lean().exec();
-  }
-
-  public static async create(
-    user: User,
-    accessTokenKey: string,
-    refreshTokenKey: string,
-    roleCode: string,
-  ): Promise<{ user: User; keystore: Keystore }> {
-    const now = new Date();
-
-    const role = await RoleModel.findOne({ code: roleCode }).select('+code').lean().exec();
-    if (!role) throw new InternalError('Role must be defined');
-
-    user.roles = [role];
-    user.createdAt = user.updatedAt = now;
-    const createdUser = await UserModel.create(user);
-    const keystore = await KeystoreRepo.create(createdUser, accessTokenKey, refreshTokenKey);
-    return {
-      user: { ...createdUser.toObject(), roles: user.roles },
-      keystore: keystore,
-    };
-  }
-
-  public static async update(
-    user: User,
-    accessTokenKey: string,
-    refreshTokenKey: string,
-  ): Promise<{ user: User; keystore: Keystore }> {
-    user.updatedAt = new Date();
-    await UserModel.updateOne({ _id: user._id }, { $set: { ...user } })
-      .lean()
-      .exec();
-    const keystore = await KeystoreRepo.create(user, accessTokenKey, refreshTokenKey);
-    return { user: user, keystore: keystore };
-  }
-
-  public static updateInfo(user: User): Promise<any> {
-    user.updatedAt = new Date();
-    return UserModel.updateOne({ _id: user._id }, { $set: { ...user } })
-      .lean()
-      .exec();
-  }
+async function exists(id: Types.ObjectId): Promise<boolean> {
+  const user = await UserModel.exists({ _id: id, status: true });
+  return user !== null && user !== undefined;
 }
+
+async function findPrivateProfileById(id: Types.ObjectId): Promise<User | null> {
+  return UserModel.findOne({ _id: id, status: true })
+    .select('+email')
+    .populate({
+      path: 'roles',
+      match: { status: true },
+      select: { code: 1 },
+    })
+    .lean<User>()
+    .exec();
+}
+
+// contains critical information of the user
+async function findById(id: Types.ObjectId): Promise<User | null> {
+  return UserModel.findOne({ _id: id, status: true })
+    .select('+email +password +roles')
+    .populate({
+      path: 'roles',
+      match: { status: true },
+    })
+    .lean()
+    .exec();
+}
+
+async function findByEmail(email: string): Promise<User | null> {
+  return UserModel.findOne({ email: email })
+    .select(
+      '+email +password +roles +gender +dob +grade +country +state +city +school +bio +hobbies',
+    )
+    .populate({
+      path: 'roles',
+      match: { status: true },
+      select: { code: 1 },
+    })
+    .lean()
+    .exec();
+}
+
+async function findFieldsById(id: Types.ObjectId, ...fields: string[]): Promise<User | null> {
+  return UserModel.findOne({ _id: id, status: true }, [...fields])
+    .lean()
+    .exec();
+}
+
+async function findPublicProfileById(id: Types.ObjectId): Promise<User | null> {
+  return UserModel.findOne({ _id: id, status: true }).lean().exec();
+}
+
+async function create(
+  user: User,
+  accessTokenKey: string,
+  refreshTokenKey: string,
+  roleCode: string,
+): Promise<{ user: User; keystore: Keystore }> {
+  const now = new Date();
+
+  const role = await RoleModel.findOne({ code: roleCode }).select('+code').lean().exec();
+  if (!role) throw new InternalError('Role must be defined');
+
+  user.roles = [role];
+  user.createdAt = user.updatedAt = now;
+  const createdUser = await UserModel.create(user);
+  const keystore = await KeystoreRepo.create(createdUser, accessTokenKey, refreshTokenKey);
+  return {
+    user: { ...createdUser.toObject(), roles: user.roles },
+    keystore: keystore,
+  };
+}
+
+async function update(
+  user: User,
+  accessTokenKey: string,
+  refreshTokenKey: string,
+): Promise<{ user: User; keystore: Keystore }> {
+  user.updatedAt = new Date();
+  await UserModel.updateOne({ _id: user._id }, { $set: { ...user } })
+    .lean()
+    .exec();
+  const keystore = await KeystoreRepo.create(user, accessTokenKey, refreshTokenKey);
+  return { user: user, keystore: keystore };
+}
+
+async function updateInfo(user: User): Promise<any> {
+  user.updatedAt = new Date();
+  return UserModel.updateOne({ _id: user._id }, { $set: { ...user } })
+    .lean()
+    .exec();
+}
+
+export default {
+  exists,
+  findPrivateProfileById,
+  findById,
+  findByEmail,
+  findFieldsById,
+  findPublicProfileById,
+  create,
+  update,
+  updateInfo,
+};
