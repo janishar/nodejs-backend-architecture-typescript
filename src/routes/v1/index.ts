319a4099046fbf1aa42af8b0dd90becbfde66355
Janishar Ali <janishar.ali@gmail.com>
2022-12-19 16:29:55 +0530

    fix signup test

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import apikey from '../../auth/apikey';
 import permission from '../../helpers/permission';
 import { Permission } from '../../database/model/ApiKey';
+import signup from './access/signup';
 import login from './access/login';
 import logout from './access/logout';
 import token from './access/token';
@@ -15,10 +16,10 @@ const router = express.Router();
 /*---------------------------------------------------------*/
 router.use(apikey);
 /*---------------------------------------------------------*/
-
 /*---------------------------------------------------------*/
 router.use(permission(Permission.GENERAL));
 /*---------------------------------------------------------*/
+router.use('/signup', signup);
 router.use('/login', login);
 router.use('/logout', logout);
 router.use('/token', token);
