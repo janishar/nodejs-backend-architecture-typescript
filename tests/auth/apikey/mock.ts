10b57170e80feb352b2a191956016aaaee2a3213
Janishar Ali <janishar.ali@gmail.com>
2022-12-19 13:23:43 +0530

    update apikey unit test

--- tests/auth/apikey/mock.ts
+++ tests/auth/apikey/mock.ts
@@ -3,12 +3,14 @@ import ApiKey from '../../../src/database/model/ApiKey';
 export const API_KEY = 'abc';
 
 export const mockFindApiKey = jest.fn(async (key: string) => {
-  if (key == API_KEY) return { key: API_KEY } as ApiKey;
+  if (key == API_KEY)
+    return {
+      key: API_KEY,
+      permissions: ['GENERAL'],
+    } as ApiKey;
   else return null;
 });
 
 jest.mock('../../../src/database/repository/ApiKeyRepo', () => ({
-  get findByKey() {
-    return mockFindApiKey;
-  },
+  findByKey: mockFindApiKey,
 }));
